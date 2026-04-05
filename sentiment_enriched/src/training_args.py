ba4014b054440c145b5731350c881149e1874b9d
liz <lizaolva@yandex.ru>
2026-04-05 22:12:06 +0300

    corrected args in sent enriched

--- sentiment_enriched/src/training_args.py
+++ sentiment_enriched/src/training_args.py
@@ -3,13 +3,16 @@ from transformers import TrainingArguments
 def train_args(learning_rate, weight_decay, batch_size, epochs):
     training_args = TrainingArguments(
     eval_strategy="epoch",
+    save_strategy="epoch",
+    logging_strategy="epoch",
     learning_rate = learning_rate,
     weight_decay = weight_decay,
     per_device_train_batch_size=batch_size,
     per_device_eval_batch_size=batch_size,
     num_train_epochs=epochs,
-    logging_strategy="epoch",
-    save_strategy="no",
+    save_total_limit=2,
+    save_only_model=True,
+    logging_steps=100,
     load_best_model_at_end=True,
     metric_for_best_model="f1"  
     )
